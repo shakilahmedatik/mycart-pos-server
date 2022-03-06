fba1ea3b368acafde07789e4d61cff1907c0e2c5
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-06 22:48:25 +0600

    Items model added

---
+++ models/itemsModel.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const itemsSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+})
+
+const itemsModel = mongoose.model('items', itemsSchema)
+module.exports = itemsModel
