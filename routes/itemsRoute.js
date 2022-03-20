119fce06f4ca8f3b8f925d01df8336b18f1334d4
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-20 17:40:29 +0600

    Route for deleting item added

--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { allItems, addItem, editItem } from '../controllers/itemsController'
+import {
+  allItems,
+  addItem,
+  editItem,
+  deleteItem,
+} from '../controllers/itemsController'
 
 const router = express.Router()
 
 router.get('/items/get-all-items', allItems)
 router.post('/items/add-items', addItem)
 router.put('/items/edit-item', editItem)
+router.delete('/items/delete-item', deleteItem)
 
 module.exports = router
