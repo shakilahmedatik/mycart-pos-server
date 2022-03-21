64eb53dcf02c66c727901a2ddca763e1433dafbf
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-21 23:36:39 +0600

    issue fixed

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    userId: { type: String, required: true },
     password: { type: String, required: true },
     verified: { type: Boolean, required: false },
   },
