0399a4023fd843049e62b6f49f1266f67f6a70a4
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-21 23:50:50 +0600

    issue fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,10 @@ import userModel from '../models/userModel'
 // Register new user
 export const register = async (req, res) => {
   try {
-    const newUser = new userModel(req.body)
+    // userId validation
+    let userExist = await userModel.findOne({ userId }).exec()
+    if (userExist) return res.status(400).send('userId is taken')
+    const newUser = new userModel({ ...req.body, verified: false })
     await newUser.save()
     res.send('Registration Successful, Please Wait For Verification!')
   } catch (error) {
@@ -15,12 +18,14 @@ export const register = async (req, res) => {
 // Login user
 export const login = async (req, res) => {
   try {
-    await userModel.findOne({
+    const user = await userModel.findOne({
       userId: req.body.userId,
       password: req.body.password,
       verified: true,
     })
-    res.send('Login Successful!')
+    if (user) {
+      res.send('Login Successful!')
+    } else res.send('Login Failed!')
   } catch (error) {
     console.log(error)
     res.status(400).json(error)
