f6baa1250dc23c4eb178aa6ef76f0ea50c94b5d3
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-26 23:42:27 +0600

    bills route updated

--- routes/billsRoute.js
+++ routes/billsRoute.js
@@ -3,6 +3,7 @@ import { chargeBill } from '../controllers/billsController'
 
 const router = express.Router()
 
+// Add bills
 router.post('/bills/charge-bill', chargeBill)
 
 module.exports = router
