e4a267ef5bc01c3b01826acf523df9745731bb19
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-27 23:21:21 +0600

    controller to get all the bills info is added

--- controllers/billsController.js
+++ controllers/billsController.js
@@ -1,5 +1,16 @@
 import billModel from '../models/billModel'
 
+// Get all the bills from database
+export const allBills = async (req, res) => {
+  try {
+    const bills = await billModel.find()
+    res.send(bills)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error)
+  }
+}
+
 // Add an item in the database.
 export const chargeBill = async (req, res) => {
   try {
